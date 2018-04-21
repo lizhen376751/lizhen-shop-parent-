3eb84e01049798b7918bb2e4694fa7954699188a
724577807@qq.com <724577807@qq.com>
2018-04-21 11:11:04 +0800

    邮件发送成功

--- lizhen-shop-message/src/main/java/com/lizhen/email/service/EmailService.java
+++ lizhen-shop-message/src/main/java/com/lizhen/email/service/EmailService.java
@@ -4,6 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.lizhen.adapter.MessageAdapter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +18,14 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class EmailService implements MessageAdapter {
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${msg.subject}")
+    private String subject;
+    @Value("${msg.text}")
+    private String text;
+    @Autowired
+    private JavaMailSender javaMailSender;
     //重写接口里面发送消息的信息
     @Override
     public void sendMsg(JSONObject body) {
@@ -21,6 +34,17 @@ public class EmailService implements MessageAdapter {
         if (StringUtils.isEmpty(email)) {
             return;
         }
-        log.info("消息服务平台发送邮件:{}",email);
+        log.info("消息服务平台发送邮件:{}", email);
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        //邮件发送者
+        simpleMailMessage.setFrom(username);
+        //邮件接受者
+        simpleMailMessage.setTo(email);
+        //设置标题
+        simpleMailMessage.setSubject(subject);
+        //设置内容
+        simpleMailMessage.setText(text.replace("{}",email));
+        //发送邮件
+        javaMailSender.send(simpleMailMessage);
     }
 }
